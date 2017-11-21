172f9bf0f2080603f0eb8c8fc5cdbb7db3b38164
Mgonand <marcos@binpar.com>
2017-11-21 14:09:51 +0100

    v2.2.6 - added sudo in deploy commands

--- scripts/linux/deploy.sh
+++ scripts/linux/deploy.sh
@@ -70,21 +70,21 @@ set -e
 APP_DIR=/opt/<%=appName %>
 
 if [[ -d /opt/nodejs ]]; then
-   ln -sf /opt/nodejs/bin/node /usr/bin/node
-   ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+   sudo ln -sf /opt/nodejs/bin/node /usr/bin/node
+   sudo ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 else
    cd /tmp
    LINK_NODE4=http://nodejs.org/dist/latest-v4.x/
    DOWNLOAD_NODE4=$(curl -s $LINK_NODE4 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz")
    LINK_NODE4=$LINK_NODE4$DOWNLOAD_NODE4
-   curl -O -L $LINK_NODE4
-   tar xvzf $DOWNLOAD_NODE4
+   sudo curl -O -L $LINK_NODE4
+   sudo tar xvzf $DOWNLOAD_NODE4
    DIR_NODE4=$(echo $DOWNLOAD_NODE4 | sed -e 's/.tar.gz//')
-   rm -rf /opt/nodejs
-   mv $DIR_NODE4 /opt/nodejs
-   ln -sf /opt/nodejs/bin/node /usr/bin/node
-   ln -sf /opt/nodejs/bin/npm /usr/bin/npm
-   rm -rf $DOWNLOAD_NODE4
+   sudo rm -rf /opt/nodejs
+   sudo mv $DIR_NODE4 /opt/nodejs
+   sudo ln -sf /opt/nodejs/bin/node /usr/bin/node
+   sudo ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+   sudo rm -rf $DOWNLOAD_NODE4
 fi
 
 # save the last known version
--- scripts/linux/deployNextJS.sh
+++ scripts/linux/deployNextJS.sh
@@ -4,21 +4,21 @@ set -e
 APP_DIR=/opt/<%=appName %>
 
 if [[ -d /opt/nodejs8 ]]; then
-   ln -sf /opt/nodejs8/bin/node /usr/bin/node
-   ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
+   sudo ln -sf /opt/nodejs8/bin/node /usr/bin/node
+   sudo ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
 else
    cd /tmp
    LINK_NODE8=http://nodejs.org/dist/latest-v8.x/
    DOWNLOAD_NODE8=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz")
    LINK_NODE8=$LINK_NODE8$DOWNLOAD_NODE8
-   curl -O -L $LINK_NODE8
-   tar xvzf $DOWNLOAD_NODE8
+   sudo curl -O -L $LINK_NODE8
+   sudo tar xvzf $DOWNLOAD_NODE8
    DIR_NODE8=$(echo $DOWNLOAD_NODE8 | sed -e 's/.tar.gz//')
-   rm -rf /opt/nodejs8
-   mv $DIR_NODE8 /opt/nodejs8
-   ln -sf /opt/nodejs8/bin/node /usr/bin/node
-   ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
-   rm -rf $DOWNLOAD_NODE8
+   sudo rm -rf /opt/nodejs8
+   sudo mv $DIR_NODE8 /opt/nodejs8
+   sudo ln -sf /opt/nodejs8/bin/node /usr/bin/node
+   sudo ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
+   sudo rm -rf $DOWNLOAD_NODE8
 fi
 
 cd $APP_DIR
