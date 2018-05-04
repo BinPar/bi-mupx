5014855988052eb1c9802cbebe227118c725531d
Mgonand <marcos@binpar.com>
2018-05-05 01:05:03 +0200

    Automatic update to lastest node version on nextjs deployments

--- scripts/linux/deployNextJS.sh
+++ scripts/linux/deployNextJS.sh
@@ -3,22 +3,23 @@ set -e
 
 APP_DIR=/opt/<%=appName %>
 
-if [[ -d /opt/nodejs8 ]]; then
-   sudo ln -sf /opt/nodejs8/bin/node /usr/bin/node
-   sudo ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
+LINK_NODE8=http://nodejs.org/dist/latest-v8.x/
+NODE_VERSION=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz" | sed -e 's/node-//g' -e 's/-linux-x64.tar.gz//g')
+if [ $NODE_VERSION != $(node -v) ]; then
+  cd /tmp
+  LINK_NODE8=http://nodejs.org/dist/latest-v8.x/
+  DOWNLOAD_NODE8=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz")
+  LINK_NODE8=$LINK_NODE8$DOWNLOAD_NODE8
+  sudo curl -O -L $LINK_NODE8
+  sudo tar xvzf $DOWNLOAD_NODE8
+  DIR_NODE8=$(echo $DOWNLOAD_NODE8 | sed -e 's/.tar.gz//')
+  sudo rm -rf /opt/nodejs8
+  sudo mv $DIR_NODE8 /opt/nodejs8
+  sudo ln -sf /opt/nodejs8/bin/node /usr/bin/node
+  sudo ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
+  sudo rm -rf $DOWNLOAD_NODE8
 else
-   cd /tmp
-   LINK_NODE8=http://nodejs.org/dist/latest-v8.x/
-   DOWNLOAD_NODE8=$(curl -s $LINK_NODE8 | grep -o '<a .*href=.*>' | sed -e 's/<a /\<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | grep 'linux' | grep "x64" | grep ".tar.gz")
-   LINK_NODE8=$LINK_NODE8$DOWNLOAD_NODE8
-   sudo curl -O -L $LINK_NODE8
-   sudo tar xvzf $DOWNLOAD_NODE8
-   DIR_NODE8=$(echo $DOWNLOAD_NODE8 | sed -e 's/.tar.gz//')
-   sudo rm -rf /opt/nodejs8
-   sudo mv $DIR_NODE8 /opt/nodejs8
-   sudo ln -sf /opt/nodejs8/bin/node /usr/bin/node
-   sudo ln -sf /opt/nodejs8/bin/npm /usr/bin/npm
-   sudo rm -rf $DOWNLOAD_NODE8
+  echo "NO actualizar!"
 fi
 
 cd $APP_DIR
